e049ad994f7ceb3c042b8f9d310093fb6f7e9d3e
Donggyu Kim <dgkim@eecs.berkeley.edu>
2015-12-03 02:57:16 -0800

    bump chisel & update scripts

--- tester/run-multi-samples.py
+++ tester/run-multi-samples.py
@@ -5,59 +5,42 @@ import os.path
 import re
 import csv
 import threading
-from subprocess import call
+from subprocess import Popen, call
+import time
 
 modules = list()
 sample_pwr = dict()
 
-class ThreadClass(threading.Thread):
-  def __init__(self, prefix, num, job):
-    threading.Thread.__init__(self)
-    self.prefix = prefix
-    self.num = num
-    self.job = job
-    self.modules = list()
-    self.sample_pwr = dict()
-    return
-
-  def run(self):
-    sample  = os.path.abspath("generated-src/" + self.prefix + "_" + str(self.num) + ".sample")
-    vcs_dir = os.path.abspath("vcs-sim-gl-par-" + str(self.job))
-    pt_dir  = os.path.abspath("pt-pwr-" + str(self.job))
-    if os.path.isfile(sample):
-      print "[JOB #" + str(self.job) + "] runs gate-level simulation (pt_dir = " + pt_dir + ")"
-      call(["make", "Top-replay-pwr", "SAMPLE=" + sample, "vcs_sim_gl_par_dir=" + vcs_dir, "pt_dir=" + pt_dir])
-      print "[JOB #" + str(self.job) + "] finishes gate-level simulation"
-      self.read_power_rpt(pt_dir, self.prefix + "_" + str(self.num))
-
-  def read_power_rpt(self, pt_dir, prefix):
-    pwr_regex = re.compile(r"""
-      \s*([\w_]*\s*(?:\([\w_]*\))?)\s*                 # Module name
-      ([-\+]?[0-9]*\.?[0-9]+(?:[eE][-\+]?[0-9]+)?)\s+  # Int Power
-      ([-\+]?[0-9]*\.?[0-9]+(?:[eE][-\+]?[0-9]+)?)\s+  # Switch Power
-      ([-\+]?[0-9]*\.?[0-9]+(?:[eE][-\+]?[0-9]+)?)\s+  # Leakage Power
-      ([-\+]?[0-9]*\.?[0-9]+(?:[eE][-\+]?[0-9]+)?)\s+  # Total Power
-      ([0-9]+\.[0-9]+)                                 # Percent
-      """, re.VERBOSE)
-    path = pt_dir + "/current-pt/reports/" + prefix + ".power.avg.max.report"
-    find = False
-    for line in open(path):
-      if not find:
-        tokens = line.split()
-        find = len(tokens) > 0 and tokens[0] == "Hierarchy"
-      else:
-        pwr_matched = pwr_regex.search(line)
-        if pwr_matched:
-          module     = pwr_matched.group(1)
-          int_pwr    = pwr_matched.group(2)
-          switch_pwr = pwr_matched.group(3)
-          leak_pwr   = pwr_matched.group(4)
-          total_pwr  = pwr_matched.group(5)
-          percent    = pwr_matched.group(6)
-          # print module, int_pwr, switch_pwr, leak_pwr, total_pwr, percent
-          if module.find("clk_gate") < 0:
-            self.modules.append(module)
-            self.sample_pwr[module] = total_pwr
+def read_power_rpt(pt_dir, prefix, first):
+  pwr_regex = re.compile(r"""
+    \s*([\w_]*\s*(?:\([\w_]*\))?)\s*                 # Module name
+    ([-\+]?[0-9]*\.?[0-9]+(?:[eE][-\+]?[0-9]+)?)\s+  # Int Power
+    ([-\+]?[0-9]*\.?[0-9]+(?:[eE][-\+]?[0-9]+)?)\s+  # Switch Power
+    ([-\+]?[0-9]*\.?[0-9]+(?:[eE][-\+]?[0-9]+)?)\s+  # Leakage Power
+    ([-\+]?[0-9]*\.?[0-9]+(?:[eE][-\+]?[0-9]+)?)\s+  # Total Power
+    ([0-9]+\.[0-9]+)                                 # Percent
+    """, re.VERBOSE)
+  path = pt_dir + "/current-pt/reports/" + prefix + ".power.avg.max.report"
+  find = False
+  for line in open(path):
+    if not find:
+      tokens = line.split()
+      find = len(tokens) > 0 and tokens[0] == "Hierarchy"
+    else:
+      pwr_matched = pwr_regex.search(line)
+      if pwr_matched:
+        module     = pwr_matched.group(1)
+        int_pwr    = pwr_matched.group(2)
+        switch_pwr = pwr_matched.group(3)
+        leak_pwr   = pwr_matched.group(4)
+        total_pwr  = pwr_matched.group(5)
+        percent    = pwr_matched.group(6)
+        # print module, int_pwr, switch_pwr, leak_pwr, total_pwr, percent
+        if module.find("clk_gate") < 0:
+          if first:
+            modules.append(module)
+            sample_pwr[module] = list()
+          sample_pwr[module].append(total_pwr)
 
 if __name__ == '__main__':
   prefix = str(sys.argv[1]) if (len(sys.argv) > 1) else "Top"
@@ -81,27 +64,45 @@ if __name__ == '__main__':
  
   """ clean up """
   for i in range(job_num):
-    call(["make", "-C", "pt-pwr-" + str(i), "claen"])
+    call(["make", "-C", "pt-pwr-" + str(i), "clean"])
+    call(["rm", os.path.abspath("vcs-sim-gl-par-" + str(i)) + "/*-pipe.vcd"])
  
   """ launch replays """
+  if not os.path.exists("logs"):
+    os.mkdirs("logs")
   first = True
-  for k in range((sample_size+job_num)/job_num):
-    threads = list()
-    for i in range(job_num):
-      t = ThreadClass(prefix, k*job_num+i, i)
-      print "launch job #" + str(i)
-      t.start()
-      threads.append(t)
+  for k in range((sample_size+job_num-1)/job_num+1):
+    ps = list()
+    logs = list()
+    startTime = time.clock()
+    for job in range(job_num):
+      num = k*job_num + job
+      sample  = os.path.abspath("generated-src/" + prefix + "_" + str(num) + ".sample")
+      vcs_dir = os.path.abspath("vcs-sim-gl-par-" + str(job))
+      pt_dir  = os.path.abspath("pt-pwr-" + str(job))
+      if os.path.exists(sample):
+        log = open("logs/" + prefix + "_" + str(num) + "_pwr.log", "w")
+        print "[JOB #" + str(job) + "] runs gate-level simulation (pt_dir = " + pt_dir + ")"
+        p = Popen(["make", "Top-replay-pwr", "SAMPLE=" + sample, "vcs_sim_gl_par_dir=" + vcs_dir, "pt_dir=" + pt_dir],
+                 stdout=log, stderr=log) 
+        ps.append(p)
+        logs.append(log)
+        time.sleep(20)
 
-    for i, t in enumerate(threads): 
-      t.join()
+    while any(p.poll() == None for p in ps):
+      pass
     
-    for t in threads:
-      for mod in t.modules:
-        if not (mod in sample_pwr):
-          modules.append(mod)
-          sample_pwr[mod] = list()
-        sample_pwr[mod].append(t.sample_pwr[mod])
+    for job in range(job_num):
+      print "[JOB #" + str(job) + "] finishes gate-level simulation"
+      num = k*job_num + job
+      sample  = os.path.abspath("generated-src/" + prefix + "_" + str(num) + ".sample")
+      if os.path.exists(sample):
+        logs[job].close()
+        pt_dir  = os.path.abspath("pt-pwr-" + str(job))
+        read_power_rpt(pt_dir, prefix + "_" + str(num), first)
+
+    print "Loop " + str(k) + " done in " + str(time.clock() - startTime) + " secs" 
+    first = False
 
   """ dump power """
   with open(prefix + "-pwr.csv", "w") as csvfile:
